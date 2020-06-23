833ae7a5dbf4528d981f015b53331cc1ed0e75f0
KukushkinDmi1riy <777Dmitrij@gmail.com>
2020-06-23 14:17:51 +0300

    Create components. Set loading...

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+  return (
+<div class="loadingio-spinner-double-ring-nhn1zljwvyf"><div class="ldio-lzeh335uzx7">
+<div></div>
+<div></div>
+<div><div></div></div>
+<div><div></div></div>
+</div></div>
+  );
+};
+
+export default Spinner;
